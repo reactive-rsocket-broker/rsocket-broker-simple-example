34b2ac634b5d8346faed5d4115a5cfdd37bf4269
linux_china <libing.chen@gmail.com>
2021-04-25 15:02:02 -0700

    RSocketRequesterApp

---
+++ rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/RSocketRequesterApp.java
@@ -0,0 +1,13 @@
+package com.alibaba.spring.boot.rsocket.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RSocketRequesterApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RSocketRequesterApp.class, args);
+    }
+
+}
