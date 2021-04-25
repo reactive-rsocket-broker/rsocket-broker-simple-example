fdc5b33f81a0c293da411f0d2b8360fcf3b8bb1a
linux_china <libing.chen@gmail.com>
2021-04-25 15:21:36 -0700

    remove cloudevents

--- rsocket-responder/src/main/java/com/alibaba/spring/boot/rsocket/demo/UserServiceImpl.java
+++ rsocket-responder/src/main/java/com/alibaba/spring/boot/rsocket/demo/UserServiceImpl.java
@@ -4,16 +4,11 @@ import com.alibaba.rsocket.RSocketService;
 import com.alibaba.user.User;
 import com.alibaba.user.UserService;
 import com.github.javafaker.Faker;
-import io.cloudevents.CloudEvent;
-import io.cloudevents.core.builder.CloudEventBuilder;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.util.Random;
-import java.util.UUID;
 
 @Service
 @RSocketService(serviceInterface = UserService.class)
@@ -23,16 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public Mono<User> findById(Integer id) {
         return Mono.just(randomUser(id));
-    }
-
-    public Mono<CloudEvent> processLoginEvent(CloudEvent loginEvent) {
-        CloudEvent event = CloudEventBuilder.v1()
-                .withId(UUID.randomUUID().toString())
-                .withSource(URI.create("https://example.com/users"))
-                .withType("com.alibaba.user.User")
-                .withData("text/plain", "Hello".getBytes(StandardCharsets.UTF_8)) //
-                .build();
-        return Mono.just(event);
     }
 
     private User randomUser(@Nullable Integer id) {
